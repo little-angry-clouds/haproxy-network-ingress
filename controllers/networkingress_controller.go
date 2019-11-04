bc7c531f5bfdfdaeeaf9a0aa4cdaf019e8cda066
Alex Pérez <alexperezpujol@disroot.org>
2019-11-04 13:29:53 +0100

    Fix incorrect creation of configmap
    It did: google.com 443:443 and should be google.com google.com:443

--- controllers/networkingress_controller.go
+++ controllers/networkingress_controller.go
@@ -112,7 +112,7 @@ func updateConfigmap(op NetworkIngressOperationRequest, configMapName types.Name
 		itemLogger = log.WithValues("network-ingress", item)
 		for _, rule := range item.Spec.Rules {
 			id := fmt.Sprintf("%s:%d:%d", rule.Name, rule.Port, rule.TargetPort)
-			configmapPart = configmap.Data["haproxy.cfg"] + fmt.Sprintf("\n\n# begining of %s\nfrontend %s\n  bind *:%d\n  mode tcp\n  use_backend %s\n\nbackend %s\n  mode tcp\n  server %s %d:%d\n# end of %s", id, id, rule.Port, id, id, rule.Host, rule.Port, rule.TargetPort, id)
+			configmapPart = configmap.Data["haproxy.cfg"] + fmt.Sprintf("\n\n# begining of %s\nfrontend %s\n  bind *:%d\n  mode tcp\n  use_backend %s\n\nbackend %s\n  mode tcp\n  server %s %s:%d\n# end of %s", id, id, rule.Port, id, id, rule.Host, rule.Host, rule.TargetPort, id)
 			configmap.Data["haproxy.cfg"] = configmapPart
 			itemLogger.V(2).Info("network-ingress configuration", "value", configmapPart)
 		}
