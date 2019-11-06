294d96071f1db0073c7042b9c4bdec6527f3afd4
Alex Pérez <alexperezpujol@disroot.org>
2019-11-06 21:31:57 +0100

    Add better documentation of the CRD

--- api/v1/networkingress_types.go
+++ api/v1/networkingress_types.go
@@ -8,31 +8,35 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 type NetworkIngressSpec struct {
-	// Important: Run "make" to regenerate code after modifying this file
+	// A list of hosts and its associated ports used to configure the Network
+	// Ingress.
 	Rules []Rule `json:"rules"`
 }
 
-// A rule is the core of a Network Ingress
-// It defines name, host, port and target port of a rule
+// A rule is the core of a Network Ingress . It defines name, host, port and target port of a rule
 type Rule struct {
+	// Name of the rule. This will be used as ID
 	Name       string `json:"name"`
+	// Host of the rule. This is the destination machine that Haproxy will conecct to.
 	Host       string `json:"host"`
+	// Port of the rule. This is the port that will be configured in the service.
 	Port       int    `json:"port"`
+	// Target port of the rule. This is the port that will be configured in the Haproxy' s configuration
 	TargetPort int    `json:"targetPort"`
 }
 
-// NetworkIngressStatus defines the observed state of NetworkIngress
+// Defines the observed state of NetworkIngress
 type NetworkIngressStatus struct {
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:path=networkingresses,shortName=ningress;ning
+// +kubebuilder:printcolumn:name="Service",type=string,JSONPath=`.spec.rules[].name`
+// +kubebuilder:printcolumn:name="Port",type=integer,JSONPath=`.spec.rules[].port`
+// +kubebuilder:printcolumn:name="TargetPort",type=integer,JSONPath=`.spec.rules[].targetPort`
 
-// NetworkIngress is the Schema for the networkingresses API
+// NetworkIngress is the Schema for the Network Ingress API
 type NetworkIngress struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
